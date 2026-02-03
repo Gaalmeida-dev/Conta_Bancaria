466c8b63c6f4e24afa4245c4cb5d66ad5bad3e7e
unknown <gabrielalmeidaescalera@gmail.com>
2026-02-03 18:39:02 -0300

    gitignore

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline_sync_1 = __importDefault(require("readline-sync"));
+console.log("Olá! Digite: " + readline_sync_1.default.question("Nome: "));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+import readlineSync from "readline-sync"; console.log("Olá! Digite: " + readlineSync.question("Nome: "));
